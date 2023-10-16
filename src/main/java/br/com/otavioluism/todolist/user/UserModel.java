cc72a7d2770875d874a8c0217c97b3902e355c88
Otávio Luis Martins <otavioluism97@hotmail.com>
2023-10-16 20:46:30 -0300

    criando conexao ao BD e criando tabela (entidade)

--- src/main/java/br/com/otavioluism/todolist/user/UserModel.java
+++ src/main/java/br/com/otavioluism/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package br.com.otavioluism.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
+@Data // annotation para ter o setter e getter configurado na classe
+@Entity(name = "tb_users") // Entity e a traducao do nome da tabela por ORM para criar no banco
 public class UserModel {
+
+  @Id // forcando a variavel ser uma chave primary key
+  @GeneratedValue(generator = "UUID") // gerar id automativo no formato UUID
+  private UUID id;
 
   private String name; 
   private String username; 
   private String password; 
+
+  @CreationTimestamp // pegando o horario atual da criacao
+  private LocalDateTime createdAt; 
 
 }
