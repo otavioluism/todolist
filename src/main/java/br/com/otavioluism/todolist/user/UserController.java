37eeb80f3f39db1169ed2841ec86d6d4e67b97cb
Otávio Luis Martins <otavioluism97@hotmail.com>
2023-10-16 21:53:40 -0300

    criando validacao de objetos e add status code

--- src/main/java/br/com/otavioluism/todolist/user/UserController.java
+++ src/main/java/br/com/otavioluism/todolist/user/UserController.java
@@ -2,6 +2,8 @@ package br.com.otavioluism.todolist.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,16 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/create")
-  public UserModel create(@RequestBody UserModel userModel) { 
+  public ResponseEntity create(@RequestBody UserModel userModel) { 
+        UserModel user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if (user != null) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe!");
+        }
+
         var userCreate = this.userRepository.save(userModel);
-        return userCreate;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreate);
+
   }
   
 }
