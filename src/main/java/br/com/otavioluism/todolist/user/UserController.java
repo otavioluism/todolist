7babb89d4b168e29e79b703c3bc819c9fa0188eb
Otávio Luis Martins <otavioluism97@hotmail.com>
2023-10-16 21:30:44 -0300

    criando repository e add dados do cliente no bd

--- src/main/java/br/com/otavioluism/todolist/user/UserController.java
+++ src/main/java/br/com/otavioluism/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.otavioluism.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UserController { 
+
+  @Autowired //anotation para realizar a construcao da clase 
+  private IUserRepository userRepository;
 
   @PostMapping("/create")
-  public void create(@RequestBody UserModel userModel) { 
-        System.out.println(userModel.getName());
+  public UserModel create(@RequestBody UserModel userModel) { 
+        var userCreate = this.userRepository.save(userModel);
+        return userCreate;
   }
   
 }
